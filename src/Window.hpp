2319e1310a19afef06d9e81d233cf915130e9ebb
Cian Jinks <cjinks99@gmail.com>
2021-08-28 16:28:01 +0100

    Fixed crashes with new ImGUI

--- src/Window.hpp
+++ src/Window.hpp
@@ -1,6 +1,10 @@
 #ifndef _WINDOW_H
 #define _WINDOW_H
 
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
